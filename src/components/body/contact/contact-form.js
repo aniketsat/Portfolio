14ab74b7c77eb165f12c78f5e68d439f13589ccc
Aniket Satapathy <aniketsatapathy193@gmail.com>
2021-11-06 22:07:11 +0530

    bug fixed

--- src/components/body/contact/contact-form.js
+++ src/components/body/contact/contact-form.js
@@ -1,44 +1,30 @@
-import emailjs from "emailjs-com";
+
 import React from 'react';
 import './contact-form.css'
 
 export default function ContactUs() {
 
-    function sendEmail(e) {
-        e.preventDefault();
-
-    emailjs.sendForm('gmail', 'template_rb4n5pl', e.target, 'user_JABO21I8Gm6sxByJH17Nu')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
-
     return(
-        <div>
-            <div className="container">
-            <form onSubmit={sendEmail} action="POST" data-netlify="true">
-                    <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
-                </form>
+        <form name="contact" method="post" className="container">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="row pt-5 mx-auto">
+                <div className="col-8 form-group mx-auto">
+                    <input type="text" className="form-control" placeholder="Name" name="name" required/>
+                </div>
+                <div className="col-8 form-group pt-2 mx-auto">
+                    <input type="email" className="form-control" placeholder="Email Address" name="email" required/>
+                </div>
+                <div className="col-8 form-group pt-2 mx-auto">
+                    <input type="text" className="form-control" placeholder="Subject" name="subject" required/>
+                </div>
+                <div className="col-8 form-group pt-2 mx-auto">
+                    <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message" required></textarea>
+                </div>
+                <div className="col-8 pt-3 mx-auto">
+                    <input type="submit" className="btn btn-info" value="Send Message"></input>
+                </div>
             </div>
-        </div>
+        </form>
+
     )
 }
