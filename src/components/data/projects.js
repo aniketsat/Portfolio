e1fb85575c27328085e6500f485ffec45416ff57
Aniket Satapathy <aniketsatapathy193@gmail.com>
2022-05-05 23:52:22 +0530

    skills and projects updated

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,39 +1,66 @@
 export const ProjectData = [
-    {
-        id: 1,
-        title: "Quoter",
-        about: "Get Quotes of some of the well-known and successful people in a single place...A RANDOM QUOTE GENERATOR for your knowlegde and motivation !",
-        tags: ["HTML", "CSS", "JavaScript", "FetchAPI", "API"],
-        demo: "https://aniketsat.github.io/Quoter/",
-        github: "https://github.com/aniketsat/Quoter",
-        image: "https://github.com/aniketsat/Quoter/blob/master/quote.jpeg?raw=true", 
-    }, 
-    {
-        id: 2,
-        title: "Picture-In-Picture",
-        about: "Read your favourite blog or documentation with watching some tutorial side-by-side using this tool.... Click the SHARE button for sharing any tabs or screen and then click START to get this working !",
-        tags: ["HTML", "CSS", "JavaScript", "FetchAPI", "API"],
-        demo: "https://aniketsat.github.io/Picture-In-Picture/",
-        github: "https://github.com/aniketsat/Picture-In-Picture",
-        image: "https://github.com/aniketsat/Picture-In-Picture/blob/master/pip.jpeg?raw=true", 
-    }, 
-    {
-        id: 3,
-        title: "Alexa",
-        about: "It is an simple AI assistant with just python using several python libraries like pyjokes, pyaudio, pywhatkit, speechrecognition etc",
-        tags: ["Python", "SpeechRecognition", "pyjokes", "pyaudio"],
-        // demo: "www.demo-link.com",
-        github: "https://github.com/aniket-1812/Alexa",
-        image: "https://d3ogm7ac91k97u.cloudfront.net/content/dam/alexa/alexa-brand-guidelines-2021-refresh-/Alexa_Logo_RGB_BLUE.png", 
-    }, 
-    {
-        id: 4,
-        title: "Breast Cancer Analysis",
-        about: "In this project logistic regression is implemented using Scikit-learn library of python. Here prediction is done using a machine learning algorithm which predicts that a patient has malignant or benign Breast Cancer Tumor considering different factors like Clump Thickness, Uniformity of Cell Size etc",
-        tags: ["Python", "Machine Learning", "Scikit-learn", "Logistic Regression"],
-        // demo: "www.demo-link.com",
-        github: "https://github.com/aniket-1812/Breast-Cancer-Analysis",
-        image: "https://miro.medium.com/max/1050/1*WmGCezZGdyhX4ahjAm1p4g.png",
-    },
-
-]
+  {
+    id: 1,
+    title: "Portfolio",
+    about: "Personal Portfolio website made using React...",
+    tags: ["React", "React Hooks", "React Router DOM"],
+    demo: "https://aniketsat.netlify.app/",
+    github: "https://github.com/aniketsat/Portfolio",
+    image:
+      "https://github.com/aniketsat/Portfolio/blob/master/home-image.png?raw=true",
+  },
+  {
+    id: 2,
+    title: "Sudoku Solver",
+    about:
+      "Generate and Solve any sudoku problem with the Sudoku-Solver made with HTML,CSS and JavaScript, solved by a simple API call...",
+    tags: ["HTML", "CSS", "JavaScript", "Axios", "RapidAPI"],
+    demo: "https://sudo-solver.netlify.app/",
+    github: "https://github.com/aniketsat/Sudoku-Solver",
+    image:
+      "https://github.com/aniketsat/Sudoku-Solver/blob/master/Sudoku-Solved.png?raw=true",
+  },
+  {
+    id: 3,
+    title: "Quoter",
+    about:
+      "Get Quotes of some of the well-known and successful people in a single place...A RANDOM QUOTE GENERATOR for your knowlegde and motivation !",
+    tags: ["HTML", "CSS", "JavaScript", "FetchAPI", "API"],
+    demo: "https://aniketsat.github.io/Quoter/",
+    github: "https://github.com/aniketsat/Quoter",
+    image:
+      "https://github.com/aniketsat/Quoter/blob/master/quote.jpeg?raw=true",
+  },
+  {
+    id: 4,
+    title: "Picture-In-Picture",
+    about:
+      "Read your favourite blog or documentation with watching some tutorial side-by-side using this tool.... Click the SHARE button for sharing any tabs or screen and then click START to get this working !",
+    tags: ["HTML", "CSS", "JavaScript", "FetchAPI", "API"],
+    demo: "https://aniketsat.github.io/Picture-In-Picture/",
+    github: "https://github.com/aniketsat/Picture-In-Picture",
+    image:
+      "https://github.com/aniketsat/Picture-In-Picture/blob/master/pip.jpeg?raw=true",
+  },
+  {
+    id: 5,
+    title: "Alexa",
+    about:
+      "It is an simple AI assistant with just python using several python libraries like pyjokes, pyaudio, pywhatkit, speechrecognition etc",
+    tags: ["Python", "SpeechRecognition", "pyjokes", "pyaudio"],
+    // demo: "www.demo-link.com",
+    github: "https://github.com/aniket-1812/Alexa",
+    image:
+      "https://d3ogm7ac91k97u.cloudfront.net/content/dam/alexa/alexa-brand-guidelines-2021-refresh-/Alexa_Logo_RGB_BLUE.png",
+  },
+  {
+    id: 6,
+    title: "Breast Cancer Analysis",
+    about:
+      "In this project logistic regression is implemented using Scikit-learn library of python. Here prediction is done using a machine learning algorithm which predicts that a patient has malignant or benign Breast Cancer Tumor considering different factors like Clump Thickness, Uniformity of Cell Size etc",
+    tags: ["Python", "Machine Learning", "Scikit-learn", "Logistic Regression"],
+    // demo: "www.demo-link.com",
+    github: "https://github.com/aniket-1812/Breast-Cancer-Analysis",
+    image: "https://miro.medium.com/max/1050/1*WmGCezZGdyhX4ahjAm1p4g.png",
+  },
+];
--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -1,97 +1,110 @@
 export const SkillsData = [
-    {
-        type: "Programming Languages",
-        list:[
-            {
-                name: "Python",
-                icon: <i class="devicon-python-plain colored"></i>,
-            },
-            {
-                name: "Java",
-                icon: <i class="devicon-java-plain colored"></i>,
-            },
-            {
-                name: "JavaScript",
-                icon: <i class="devicon-javascript-plain colored"></i>,
-            },
-        ]
-    },
-    {
-        type: "Technologies",
-        list:[
-            {
-                name: "Machine Learning",
-                icon: <i class="fas fa-laptop-code"></i>,
-            },
-            {
-                name: "Web Development",
-                icon: <i class="fas fa-globe"></i>,
-            },
-        ]
-    },
-    {
-        type: "Frontend",
-        list:[
-            {
-                name: "React",
-                icon: <i class="devicon-react-original colored"></i>,
-            },
-            {
-                name: "Bootstrap",
-                icon: <i class="devicon-bootstrap-plain colored"></i>,
-            },
-        ]
-    },
-    {
-        type: "Backend",
-        list:[
-            {
-                name: "NodeJS",
-                icon: <i class="devicon-nodejs-plain colored"></i>,
-            },
-            {
-                name: "express",
-                icon: <i class="devicon-express-original colored"></i>,
-            },
-        ]
-    },
-    {
-        type: "Data-Science Libraries",
-        list:[
-            {
-                name: "Numpy",
-                icon: <i class="devicon-numpy-original colored"></i>,
-            },
-            {
-                name: "Pandas",
-                icon: <i class="devicon-pandas-original colored"></i>,
-            },
-        ]
-    },
-    {
-        type: "Database",
-        list:[
-            {
-                name: "MySQL",
-                icon: <i class="devicon-mysql-plain-wordmark colored"></i>,
-            },
-            {
-                name: "MongoDB",
-                icon: <i class="devicon-mongodb-plain-wordmark colored"></i>,
-            },
-            {
-                name: "Firebase",
-                icon: <i class="devicon-firebase-plain colored"></i>,
-            },
-        ]
-    },
-    {
-        type: "Cloud Platforms",
-        list:[
-            {
-                name: "Google Cloud",
-                icon: <i class="devicon-googlecloud-plain colored"></i>,
-            },
-        ]
-    },
-]
+  {
+    type: "Programming Languages",
+    list: [
+      {
+        name: "Python",
+        icon: <i class="devicon-python-plain colored"></i>,
+      },
+      {
+        name: "Java",
+        icon: <i class="devicon-java-plain colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Technologies",
+    list: [
+      {
+        name: "Machine Learning",
+        icon: <i class="fas fa-laptop-code"></i>,
+      },
+      {
+        name: "Web Development",
+        icon: <i class="fas fa-globe"></i>,
+      },
+    ],
+  },
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "React",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "Bootstrap",
+        icon: <i class="devicon-bootstrap-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    list: [
+      {
+        name: "NodeJS",
+        icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+      {
+        name: "express",
+        icon: <i class="devicon-express-original colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Data-Science Libraries",
+    list: [
+      {
+        name: "Numpy",
+        icon: <i class="devicon-numpy-original colored"></i>,
+      },
+      {
+        name: "Pandas",
+        icon: <i class="devicon-pandas-original colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Database",
+    list: [
+      {
+        name: "MongoDB",
+        icon: <i class="devicon-mongodb-plain-wordmark colored"></i>,
+      },
+      {
+        name: "Firebase",
+        icon: <i class="devicon-firebase-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Cloud Platforms",
+    list: [
+      {
+        name: "Google Cloud Platform",
+        icon: <i class="devicon-googlecloud-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Tools",
+    list: [
+      {
+        name: "Git",
+        icon: <i class="devicon-git-plain colored"></i>,
+      },
+      {
+        name: "Github",
+        icon: <i class="devicon-github-original colored"></i>,
+      },
+      {
+        name: "VS Code",
+        icon: <i class="devicon-vscode-plain colored"></i>,
+      },
+    ],
+  },
+];
