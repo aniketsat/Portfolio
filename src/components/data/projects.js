dbaaf8809798b682ea29b53a5a7955045fd84f1f
Aniket Satapathy <85563772+aniketsat@users.noreply.github.com>
2022-08-29 22:38:57 +0530

    Update projects.js

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -11,6 +11,28 @@ export const ProjectData = [
   },
   {
     id: 2,
+    title: "Name It",
+    about:
+      "Get a name for your next project ✨",
+    tags: ["ReactJS", "CSS", "JavaScript", "NPM"],
+    demo: "https://nameittt.netlify.app/",
+    github: "https://github.com/aniketsat/NameIttt",
+    image:
+      "",
+  },
+  {
+    id: 3,
+    title: "Weatheryyy",
+    about:
+      "Find about the weather in your nearest town...",
+    tags: ["HTML", "CSS", "JavaScript", "FetchAPI", "API"],
+    demo: "https://weatheryyy.netlify.app/",
+    github: "https://github.com/aniketsat/Weather-App",
+    image:
+      "",
+  },
+  {
+    id: 4,
     title: "Sudoku Solver",
     about:
       "Generate and Solve any sudoku problem with the Sudoku-Solver made with HTML,CSS and JavaScript, solved by a simple API call...",
@@ -21,7 +43,7 @@ export const ProjectData = [
       "https://github.com/aniketsat/Sudoku-Solver/blob/master/Sudoku-Solved.png?raw=true",
   },
   {
-    id: 3,
+    id: 5,
     title: "Quoter",
     about:
       "Get Quotes of some of the well-known and successful people in a single place...A RANDOM QUOTE GENERATOR for your knowlegde and motivation !",
@@ -32,7 +54,7 @@ export const ProjectData = [
       "https://github.com/aniketsat/Quoter/blob/master/quote.jpeg?raw=true",
   },
   {
-    id: 4,
+    id: 6,
     title: "Picture-In-Picture",
     about:
       "Read your favourite blog or documentation with watching some tutorial side-by-side using this tool.... Click the SHARE button for sharing any tabs or screen and then click START to get this working !",
@@ -41,26 +63,5 @@ export const ProjectData = [
     github: "https://github.com/aniketsat/Picture-In-Picture",
     image:
       "https://github.com/aniketsat/Picture-In-Picture/blob/master/pip.jpeg?raw=true",
-  },
-  {
-    id: 5,
-    title: "Alexa",
-    about:
-      "It is an simple AI assistant with just python using several python libraries like pyjokes, pyaudio, pywhatkit, speechrecognition etc",
-    tags: ["Python", "SpeechRecognition", "pyjokes", "pyaudio"],
-    // demo: "www.demo-link.com",
-    github: "https://github.com/aniket-1812/Alexa",
-    image:
-      "https://d3ogm7ac91k97u.cloudfront.net/content/dam/alexa/alexa-brand-guidelines-2021-refresh-/Alexa_Logo_RGB_BLUE.png",
-  },
-  {
-    id: 6,
-    title: "Breast Cancer Analysis",
-    about:
-      "In this project logistic regression is implemented using Scikit-learn library of python. Here prediction is done using a machine learning algorithm which predicts that a patient has malignant or benign Breast Cancer Tumor considering different factors like Clump Thickness, Uniformity of Cell Size etc",
-    tags: ["Python", "Machine Learning", "Scikit-learn", "Logistic Regression"],
-    // demo: "www.demo-link.com",
-    github: "https://github.com/aniket-1812/Breast-Cancer-Analysis",
-    image: "https://miro.medium.com/max/1050/1*WmGCezZGdyhX4ahjAm1p4g.png",
   },
 ];
